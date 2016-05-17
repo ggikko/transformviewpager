3f6e02d6da4f789f0510c8c01a7b07fd7d5c749a
Jihong Park <ggikko@hotmail.com>
2016-05-17 16:42:14 +0900

    add pager

--- TransformApp/app/src/main/java/ggikko/me/transformapp/MainActivity.java
+++ TransformApp/app/src/main/java/ggikko/me/transformapp/MainActivity.java
@@ -1,13 +1,150 @@
 package ggikko.me.transformapp;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.VideoView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static Uri first;
+    private static Uri second;
+    private static Uri third;
+
+    private final GgikkoPagerAdapter adapter = new GgikkoPagerAdapter();
+
+    @Bind(R.id.viewpager) ViewPager viewpager;
+
+    VideoView one_video;
+    VideoView two_video;
+    VideoView three_video;
+    VideoView four_video;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        viewpager.setAdapter(adapter);
+        viewpager.addOnPageChangeListener(pageChangeListener);
+        viewpager.setOffscreenPageLimit(4);
+
+
     }
+
+    ViewPager.SimpleOnPageChangeListener pageChangeListener = new ViewPager.SimpleOnPageChangeListener(){
+
+        private int currentPosition = -1;
+        private int startTimeForTab;
+        
+        @Override
+        public void onPageSelected(int position) {
+            switch (position){
+                default:
+                    throw new IllegalArgumentException("error");
+                case 0:{
+                    return;
+                }
+                case 1:{
+                    return;
+                }
+                case 2:{
+                    return;
+                }
+                case 3:{
+                    return;
+                }
+                case 4:{
+                    return;
+                }
+            }
+        }
+    };
+
+    /** start video */
+    private void playVideo(VideoView videoView){
+        videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                return false;
+            }
+        });
+        try {
+            videoView.start();
+        }catch(Throwable e){
+            Log.e("ggikko", "비디오 실행 불가");
+        }
+    }
+
+    /** stop video */
+    private void stopVideo(VideoView videoView){
+        if(videoView == null){
+            return;
+        }
+        videoView.seekTo(0);
+        videoView.pause();
+    }
+
+    private class GgikkoPagerAdapter extends PagerAdapter{
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+
+            switch (position) {
+                default:
+                    ViewGroup layout0 = (ViewGroup) LayoutInflater.from(MainActivity.this).inflate(R.layout.onepager, container,false);
+                    container.addView(layout0);
+                    return layout0;
+                case 0:
+                    ViewGroup layout = (ViewGroup) LayoutInflater.from(MainActivity.this).inflate(R.layout.onepager, container,false);
+                    container.addView(layout);
+                    one_video = (VideoView) findViewById(R.id.one_video);
+                    return layout;
+                case 1:
+                    ViewGroup layout2 = (ViewGroup) LayoutInflater.from(MainActivity.this).inflate(R.layout.twopager, container,false);
+                    container.addView(layout2);
+                    two_video = (VideoView) findViewById(R.id.two_video);
+                    return layout2;
+                case 2:
+                    ViewGroup layout3 = (ViewGroup) LayoutInflater.from(MainActivity.this).inflate(R.layout.threepager, container,false);
+                    container.addView(layout3);
+                    three_video = (VideoView) findViewById(R.id.three_video);
+                    return layout3;
+                case 3:
+                    ViewGroup layout4 = (ViewGroup) LayoutInflater.from(MainActivity.this).inflate(R.layout.onepager, container,false);
+                    container.addView(layout4);
+                    four_video = (VideoView) findViewById(R.id.four_video);
+                    return layout4;
+            }
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+    }
+
+
 }
